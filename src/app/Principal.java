a5260dd982de6c9c78624b887e55b8ceb3011999
wallaC-137 <wallace_031@hotmail.com>
2023-08-07 19:54:26 -0300

    Initial commit

---
+++ src/app/Principal.java
@@ -0,0 +1,13 @@
+package app;
+
+
+public class Principal {
+
+  public static void main(String[] args) {
+    for (int i = 0; i <= 10; i++) {
+      System.out.println("Olá Mundo Cruel! " + i);
+    }
+  }
+}
+
+
